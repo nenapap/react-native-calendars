b5303463ef93cfa2754db7f8b226afecaae1cb64
nenapap <31856725+nenapap@users.noreply.github.com>
2018-03-30 18:02:55 +0300

    Add files via upload

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -7,14 +7,14 @@ export default function getStyle(theme={}) {
   const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     container: {
-      paddingLeft: 5,
-      paddingRight: 5,
+      paddingLeft: 0,
+      paddingRight: 0,
       flex: 1,
       backgroundColor: appStyle.calendarBackground
     },
     week: {
-      marginTop: 7,
-      marginBottom: 7,
+      marginTop: 10,
+      marginBottom: 10,
       flexDirection: 'row',
       justifyContent: 'space-around'
     },
